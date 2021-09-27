4572cf3ee885c24cce40eb9a6cb4be70ab670e4c
AfiyaBorkar <78202078+AfiyaBorkar@users.noreply.github.com>
2021-09-27 22:36:20 +0530

    created a Addition java file
    by afiya borkar

---
+++ Addition.java
@@ -0,0 +1,15 @@
+import java.util.*;  
+class Addition 
+{  
+public static void main(String[] args)  
+{  
+Scanner sc= new Scanner(System.in); //System.in is a standard input stream  
+System.out.println("**** Addition *****");  
+System.out.println("Enter the number 1: ");
+int a= sc.nextInt();              //reads string 
+System.out.println("Enter the number 2: ");
+int b= sc.nextInt(); 
+int c = a+b;
+System.out.print("Addition: "+c);             
+}  
+}
